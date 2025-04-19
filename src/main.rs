05690c0470f8716d93af2e37147b852150245b7f
Damien Afienko <dafienko@roblox.com>
2025-04-18 17:17:54 -0700

    implement clap

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 mod scan_job;
 
-use std::env;
-use std::path::PathBuf;
+use clap::Parser;
 
 fn main() {
-    let input_path: PathBuf = env::args()
-        .nth(1)
-        .map(PathBuf::from)
-        .unwrap_or_else(|| env::current_dir().expect("Failed to get current dir"));
+    let args = scan_job::scan_job_args::ScanJobArgs::parse();
 
-    scan_job::scan_dir(input_path, None);
+    scan_job::scan_dir(args);
 
     // for portion in scan_job::PortionColors::iter() {
     //     println!("{}", color_portion("█".repeat(10), portion));
